053680b433fc7dade687dc68ae0814d2750f6231
AntonFritsche <antonfritschedresden@gmail.com>
2023-08-16 11:57:48 +0200

    updated handleUser-interface_user: sql con

--- VeReRo-Gruppe/JavaScript/handleUser-interface_admin.js
+++ VeReRo-Gruppe/JavaScript/handleUser-interface_admin.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
     host: "localhost",
-    user: "root",
+    user: "SommerCamp",
     password: "",
     database: ""
   });
