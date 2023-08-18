c87afb4a5a6e81e1265acc5d61d46ef0dc4db27e
--global <--global>
2023-08-18 13:20:26 +0200

    deleted DB button, finished edit user

--- Docker/htdocs/updateuser.php
+++ Docker/htdocs/updateuser.php
@@ -12,15 +12,30 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$lastname=$_POST['lastname'];
 
-$sql = "UPDATE Nutzerdaten SET lastname='$lastname' WHERE user_id = " . $_SESSION['user_id'];
+
+$lastname = $_POST['lastname'];
+
+$sql = "UPDATE Nutzerdaten SET
+    anrede='". $_POST['anrede'] . "',
+    lastname='" . $_POST['lastname'] ."',
+    firstname='". $_POST['firstname'] ."',
+    unternehmen='". $_POST['unternehmen'] ."',
+    stellenname='". $_POST['stellenname'] ."',
+    email='". $_POST['email'] ."',
+    tel_number='". $_POST['tel_number'] ."',
+    beschreibung='". $_POST['beschreibung'] ."' WHERE user_id = " . $_SESSION['user_id'];
+
+
+
+
+// var_dump($_POST);
 
 if ($conn->query($sql) === TRUE) {
+    header("Location: /profilepages/Profil.html");
   echo "Record updated successfully";
 } else {
   echo "Error updating record: " . $conn->error;
 }
 
 $conn->close();
-?> 
