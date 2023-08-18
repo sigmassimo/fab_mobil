b286eaefb5893937933efe9749f96ee1ff041b8c
--global <--global>
2023-08-18 11:29:15 +0200

    added profil_bearbeiten.js

--- Docker/htdocs/edituser.php
+++ Docker/htdocs/edituser.php
@@ -1,33 +1,30 @@
 <?php
+        session_start();
+        $servername = "host.docker.internal";
+        $username = "SommerCamp";
+        $password = "Sommer2023";
+        $dbname = "SommerCamp";
+        
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        
+        // Check connection
+        if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+        }
+        // echo "Connected successfully";
+        
+        
+        $sql = "SELECT firstname, lastname, rollen_id, email, tel_number, stellenname, unternehmen, anrede, beschreibung FROM Nutzerdaten WHERE user_id = " . $_SESSION['user_id'];
+        $result = $conn->query($sql);
 
-$servername = "host.docker.internal";
-$username = "SommerCamp";
-$password = "Sommer2023";
-$dbname = "SommerCamp";
-
-// echo '<form id="formular" action="/index.php" method="post"><br>
-// Name: <input type="number" name="user_id"><br>
-// E-mail: <input type="number" name="veranstaltungs_id"><br>
-// <button name="sent" value="1">Eintragen</button>
-// </form>';
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-// echo "Connected successfully";
-
-
-$sql = "INSERT INTO Teilnehmer (user_id, veranstaltungs_id)
-VALUES ($_POST[user_id], $_POST[veranstaltungs_id])";
-
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
-$conn->close();
-?>
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+              echo json_encode($row);
+              die();
+            }
+          } else {
+            echo "0 results";
+          }
+          $conn->close();
---
+++ frontend/profilepages/Profil_bearbeiten.js
@@ -0,0 +1,17 @@
+async function getProfile(){
+    const request = await fetch("/api/edituser.php");
+    const data = await request.json()
+
+    console.log(data)
+
+    document.querySelector("#anrede").value = data.anrede
+    document.querySelector("#lastname").value = data.lastname
+    document.querySelector("#firstname").value = data.firstname
+    document.querySelector("#unternehmen").value = data.unternehmen
+    document.querySelector("#stellenname").value = data.stellenname
+    document.querySelector("#email").value = data.email
+    document.querySelector("#tel_number").value = data.tel_number
+    document.querySelector("#beschreibung").value = data.beschreibung
+}
+
+getProfile();
