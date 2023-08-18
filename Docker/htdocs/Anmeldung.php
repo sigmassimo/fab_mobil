f99d74218e744c8c58303b0e567ad3d4302116c6
Konrad <k.iwanzeck@gmx.de>
2023-08-18 10:43:21 +0200

    Fixed Jonas' smort idea

--- Docker/htdocs/Anmeldung.php
+++ Docker/htdocs/Anmeldung.php
@@ -12,7 +12,6 @@ $passwort = hash('sha256', $passwort);
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
@@ -26,7 +25,7 @@ if ($result->num_rows == 0) {
 } else {
     session_start();
     while($row = $result->fetch_assoc()) {
-      $_SESSION['user_id']= $result->fetch_assoc()["user_id"];  //*user id die in $sql=..... selcetiert wird */;
+      $_SESSION['user_id']= $row["user_id"];  //*user id die in $sql=..... selcetiert wird */;
       header("Location: /Kalenderpage/kalender4.html");
       die();
     }
