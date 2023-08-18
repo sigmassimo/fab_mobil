2e3a97026acfbb2588184efd5c873c2079e7221a
AntonFritsche <antonfritschedresden@gmail.com>
2023-08-18 09:46:11 +0200

    in progress: adminregistration con with database

---
+++ frontend/adminpages/JavaScript/adminregistration.js
@@ -0,0 +1,19 @@
+const jshashes = require("jshashes");
+
+
+function adminregistration(){
+    //get the input text from the inputs and prepare for psoting into the tabl Nutzerdaten in the db
+    let anrede_input_text = document.getElementById("anrede").value;
+    let vorname_input_text = document.getElementById("vorname").value;
+    let nachname_input_text = document.getElementById("nachname").value;
+    let job_input_text = document.getElementById("job").value;
+    let company_input_text = document.getElementById("firma").value;
+    let email_input_text = document.getElementById("email").value;
+    let tel_input_text = document.getElementById("tel").value;
+    // let pic_input_image = document.getElementById("pic").value;
+    let password_input_text = document.getElementById("password");
+
+
+
+
+};
